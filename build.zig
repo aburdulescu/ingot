772b1219cdb2f95fec3462bcca92ae7f2baff747
Andrei Burdulescu <andrei.burdulescu@pm.me>
2025-09-20 18:03:39 +0300

    Add strip option

--- build.zig
+++ build.zig
@@ -4,6 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const strip = b.option(bool, "strip", "Omit debug information") orelse switch (optimize) {
+        .Debug, .ReleaseSafe => false,
+        .ReleaseFast, .ReleaseSmall => true,
+    };
+
     const exe = b.addExecutable(.{
         .name = "ingot",
         .root_module = b.createModule(.{
@@ -11,6 +16,7 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .optimize = optimize,
             .single_threaded = true,
+            .strip = strip,
         }),
     });
 

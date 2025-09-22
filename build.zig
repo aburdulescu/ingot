fcc73d628f6ce40ffe8015fe740d3dfa419db41d
Andrei Burdulescu <andrei.burdulescu@pm.me>
2025-09-22 15:34:21 +0300

    Add lz4 source code to be used

--- build.zig
+++ build.zig
@@ -9,15 +9,21 @@ pub fn build(b: *std.Build) void {
         .ReleaseFast, .ReleaseSmall => true,
     };
 
+    var mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .single_threaded = true,
+        .strip = strip,
+        .link_libc = true,
+    });
+    mod.addCSourceFiles(.{
+        .files = &[_][]const u8{"lz4.c"},
+    });
+
     const exe = b.addExecutable(.{
         .name = "ingot",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .single_threaded = true,
-            .strip = strip,
-        }),
+        .root_module = mod,
     });
 
     b.installArtifact(exe);
